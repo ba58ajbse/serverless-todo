f1802b1556e209ff9647c2713fe1ffbe6e1a10ac
Administrator <admin@example.com>
2021-07-05 20:41:38 +0900

    add get todo function

---
+++ functions/get.js
@@ -0,0 +1,42 @@
+const AWS = require('aws-sdk');
+const dynamodb = process.env.IS_OFFLINE
+  ? new AWS.DynamoDB.DocumentClient({ region: 'localhost', endpoint: 'http://localhost:8000' })
+  : new AWS.DynamoDB.DocumentClient();
+const TableName = 'todos';
+
+module.exports.handler = async (event) => {
+  const headers = {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+  };
+  let statusCode = 200;
+  let body;
+
+  const id = event.pathParameters.id;
+
+  if (!id) {
+    return {
+      statusCode: 403,
+      body: JSON.stringify('Forbidden'),
+    };
+  }
+
+  const params = {
+    TableName,
+    Key: { id },
+  };
+
+  try {
+    const res = await dynamodb.get(params).promise();
+    body = JSON.stringify(res.Item);
+  } catch (error) {
+    statusCode = error.statusCode;
+    body = JSON.stringify({ statusCode, error: error.message });
+  }
+
+  return {
+    statusCode,
+    body,
+    headers,
+  };
+};
